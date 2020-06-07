2f698ef9f37c3c7054106df8efe3c76ca0e03f71
Rei-Kigoshi <rei.kigoshi@kellogg.northwestern.edu>
2020-06-07 01:34:25 +0000

    web is done

--- app/controllers/magic_controller.rb
+++ app/controllers/magic_controller.rb
@@ -5,6 +5,21 @@ class MagicController < ApplicationController
   end
 
   def web_results
+    require "google/cloud/vision"
+  
+    image_annotator = Google::Cloud::Vision::ImageAnnotator.new
+  
+    uploaded_data = params.fetch("uploaded_image")
+
+    a_file = File.open(uploaded_data)
+
+    vision_api_results = image_annotator.web_detection({ :image => a_file })
+
+    @responses = vision_api_results.responses
+
+    @first_result = vision_api_results.responses.at(0)
+
+
     render({ :template => "magic_templates/web_results.html.erb"})
   end
 
